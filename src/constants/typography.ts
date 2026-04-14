d5f8b8e1725045fd6433374d6c8f65a35bb5f1d6
Samuel-Robert <samuelroberty95@gmail.com>
2026-04-14 13:43:10 -0300

    Style: tipografia visual sendo unico podendo alterar somente nesse arquivo

---
+++ src/constants/typography.ts
@@ -0,0 +1,19 @@
+export const Typography = {
+
+    sizes: {
+        xs: 11, 
+        sm: 13, 
+        md: 15,
+        lg: 17,
+        xl: 20,
+        xxl: 24,
+        xxxl: 30,
+    },
+
+    weights: {
+        regular: '400' as const,
+        medium: '500' as const,
+        semibold: '600' as const,
+        bold: '700' as const,
+    },
+} as const;
